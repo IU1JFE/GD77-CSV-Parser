818a482d7d2a92fafbc73d908946223547cb4abe
IU1JFE <47338441+IU1JFE@users.noreply.github.com>
2019-02-07 20:32:45 +0100

    Add files via upload

---
+++ GD77-CSV-Parser-V-0/src/main/java/GD77_CSV_Parser.java
@@ -0,0 +1,94 @@
+
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+public class GD77_CSV_Parser 
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+		 try {
+	            File excel = new File("C://temp/ponti.xlsx");
+	            File csv = new File("C://temp/out.csv");
+	            FileInputStream fis = new FileInputStream(excel);
+	            XSSFWorkbook book = new XSSFWorkbook(fis);
+	            XSSFSheet sheet = book.getSheetAt(0);
+
+	            Iterator<Row> itr = sheet.iterator();
+
+	            // Iterating over Excel file in Java
+	            while (itr.hasNext()) 
+	            {
+	                Row row = itr.next();
+
+	                // Iterating over each column of Excel file
+	                Iterator<Cell> cellIterator = row.cellIterator();
+	                while (cellIterator.hasNext()) 
+	                {
+
+	                    Cell cell = cellIterator.next();
+
+	                    switch (cell.getCellType())
+	                    {
+	                    case Cell.CELL_TYPE_STRING:
+	                        System.out.print(cell.getStringCellValue() + "\t");
+	                        break;
+	                    case Cell.CELL_TYPE_NUMERIC:
+	                        System.out.print(cell.getNumericCellValue() + "\t");
+	                        break;
+	                    case Cell.CELL_TYPE_BOOLEAN:
+	                        System.out.print(cell.getBooleanCellValue() + "\t");
+	                        break;
+	                    default:
+
+	                    }
+	                }
+	                System.out.println("");
+	            }
+	            
+	            
+	            FileOutputStream os = new FileOutputStream(csv);
+	            
+	            ///
+	            
+	            
+	            
+	            // Close workbook, OutputStream and Excel file to prevent leak
+	            os.close();
+	            book.close();
+	            fis.close();
+
+		 	} 
+		 	catch (FileNotFoundException fe)
+		 	{
+	            fe.printStackTrace();
+	        } 
+		 	catch (IOException ie) 
+		 	{
+	            ie.printStackTrace();
+	        }
+	}
+}
+		
+		
+		
+	
